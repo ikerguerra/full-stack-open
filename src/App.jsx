102debbfead904a61e23e88e47d786b9411a85f1
ikerguerra <ikerguerra2000@gmail.com>
2024-05-30 20:47:43 +0200

    Parte 1 - Un estado mas complejo - Ejercicios 1.12-1.14

--- src/App.jsx
+++ src/App.jsx
@@ -1,95 +1,47 @@
-import { useState } from "react"
-
-const Button = (props) => {
-  return (
-    <button onClick={props.onClick}>{props.text}</button>
-  )
-}
-
-const Statistics = (props) => {
-  return (
-    <StatisticsLine text={props.text} value={props.value} />
-  )
-}
-
-const StatisticsLine = (props) => {
-  return (
-    <tr>
-      <td>{props.text}</td>
-      <td>{props.value}</td>
-    </tr>
-  )
-}
+import { useState } from 'react'
 
 const App = () => {
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
-  const [all, setAll] = useState(0)
-  const [average, setAverage] = useState(0)
-  const [positive, setPositive] = useState(0)
+  const anecdotes = [
+    'If it hurts, do it more often.',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
+    'The only way to go fast, is to go well.'
+  ]
 
-  const handleClickGood = () => {
-    const updatedGood = good + 1
-    const updatedAll = all + 1
+  const [selected, setSelected] = useState(0)
+  const [points, setPoints] = useState(new Array(8).fill(0))
+  const [bestAnecdote, setBestAnecdote] = useState(0)
 
-    setGood(updatedGood)
-    setAll(updatedAll)
-    setAverage((updatedGood - bad) / updatedAll)
-    setPositive((updatedGood * 100) / updatedAll)
+  const handleVotes = () => {
+    const copy = [...points]
+
+    copy[selected] += 1
+
+    setPoints(copy)
+    setBestAnecdote(copy.findIndex((v) => v === Math.max(...copy)))
   }
 
-  const handleClickNeutral = () => {
-    const updatedAll = all + 1
+  const handleAnecdotes = () => {
+    const random = Math.floor(Math.random() * 8)
 
-    setNeutral(neutral + 1)
-    setAll(updatedAll)
-    setAverage((good - bad) / updatedAll)
-    setPositive((good * 100) / updatedAll)
+    setSelected(random)
   }
 
-  const handleClickBad = () => {
-    const updatedBad = bad + 1
-    const updatedAll = all + 1
-
-    setBad(updatedBad)
-    setAll(updatedAll)
-    setAverage((good - updatedBad) / updatedAll)
-    setPositive((good * 100) / updatedAll)
-  }
-
-  if (all > 0) {
-    return (
-      <div>
-        <h1>give feedback</h1>
-        <Button onClick={handleClickGood} text="good" />
-        <Button onClick={handleClickNeutral} text="neutral" />
-        <Button onClick={handleClickBad} text="bad" />
-        <h1>statistics</h1>
-        <table>
-          <tbody>
-            <Statistics text='good' value={good} />
-            <Statistics text='neutral' value={neutral} />
-            <Statistics text='bad' value={bad} />
-            <Statistics text='all' value={good + neutral + bad} />
-            <Statistics text='average' value={average.toFixed(1)} />
-            <Statistics text='positive' value={positive.toFixed(1) + '%'} />
-          </tbody>
-        </table>
-      </div>
-    ) 
-  } else {
-    return (
-      <div>
-        <h1>give feedback</h1>
-        <Button onClick={handleClickGood} text="good" />
-        <Button onClick={handleClickNeutral} text="neutral" />
-        <Button onClick={handleClickBad} text="bad" />
-        <h1>statistics</h1>
-        <p>No feedback given</p>
-      </div>
-    ) 
-  }
+  return (
+    <div>
+      <h1>Anecdote of the day</h1>
+      <p>{anecdotes[selected]}</p>
+      <button onClick={handleVotes}>vote</button>
+      <button onClick={handleAnecdotes}>Next anecdote</button>
+      <h1>Anecdote with most votes</h1>
+      <p>{anecdotes[bestAnecdote]}</p>
+      <p>has {Math.max(...points)} votes</p>
+    </div>
+  )
 }
 
 export default App
